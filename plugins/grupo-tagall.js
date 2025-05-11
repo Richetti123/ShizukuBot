89f41157e9b1024d174126bceb9707bbacbb0b62
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-05-11 03:34:53 +0200

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -6,8 +6,8 @@ throw false
 
 if (command == 'tagall' || command == 'invocar' || command == 'todos' || command == 'invocación' || command == 'invocacion') {
 let pesan = args.join` `
-let oi = `@richetti_123 ${pesan}`
-let teks = `${global.packname} te invoca despiertate😡!!\n\n${oi}\n\n`
+let oi = `@ivessita_xn ${pesan}`
+let teks = `${global.packname} te invoca, despiertate! 💖\n\n${oi}\n\n`
 for (let mem of participants) {
 teks += `🩶 @${mem.id.split('@')[0]}\n`}
 teks += `${global.packname}`
